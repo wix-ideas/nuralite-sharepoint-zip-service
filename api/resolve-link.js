fe76646de151e8a871afd0be394103172b66f82b
wix-ideas <hello@wixideas.com>
2025-06-05 12:25:59 +0100

    Update resolve-link.js

--- api/resolve-link.js
+++ api/resolve-link.js
@@ -1,22 +1,24 @@
-// File: /api/resolve-link.js
-
 import axios from 'axios';
 
 const TENANT_ID = '6ecbe87a-bf55-407f-b022-cab8b9faff30';
 const CLIENT_ID = '7a977f99-3397-4207-a81d-508c96dcc655';
 const CLIENT_SECRET = 'jHY8Q~Aco0L81PBneYnSGLleWLx8eXot7fExkcpM';
-
 const TOKEN_URL = `https://login.microsoftonline.com/${TENANT_ID}/oauth2/v2.0/token`;
 
 export default async function handler(req, res) {
-  // CORS headers
+  // CORS headers must be set FIRST
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
-  if (req.method === 'OPTIONS') return res.status(200).end();
+  // Early return for preflight
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
 
-  if (req.method !== 'POST') return res.status(405).send('Method Not Allowed');
+  if (req.method !== 'POST') {
+    return res.status(405).send('Method Not Allowed');
+  }
 
   const { encodedId } = req.body;
   if (!encodedId) return res.status(400).send('Missing encodedId');
@@ -52,9 +54,7 @@ async function getGraphToken() {
       scope: 'https://graph.microsoft.com/.default',
       grant_type: 'client_credentials'
     }),
-    {
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
-    }
+    { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }
   );
 
   return response.data.access_token;

4e9bddf846b8c5da431d223f55c18cbfec4716aa
wix-ideas <hello@wixideas.com>
2025-06-05 12:26:47 +0100

    Update zip-files.js

--- api/zip-files.js
+++ api/zip-files.js
@@ -22,7 +22,6 @@ export const config = {
 };
 
 export default async function handler(req, res) {
-  // CORS headers
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
